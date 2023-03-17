9bb9758dd692377d22cea52879e60fff57eeea3a
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2023-03-17 12:37:26 +0300

    Add files via upload

---
+++ repos/Lab2/Lab2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args )
+    {
+        Console.WriteLine("введите колличсктво строк");
+        string? string_K = Console.ReadLine();
+        Console.WriteLine("введите колличсктво столбцов");
+        string? string_M = Console.ReadLine();
+
+        int int_K = int.Parse(string_K);
+        int int_M = int.Parse(string_M);
+
+        int[,] num_array = new int[int_K, int_M];
+
+        var rand = new Random();
+
+        for (int i = 0; i < int_K; i++)
+        {
+            for (int j = 0; j < int_M; j++)
+            {
+                num_array[i, j] = rand.Next(0, 100);
+            }
+        }
+        for (int i = 0; i < int_K; i++)
+        {
+            for (int j = 0; j < int_M; j++)
+            {
+                Console.WriteLine($"K: {i+1}, M: {j+1}, Num: {num_array[i, j]}");
+            }
+        }
+        
+        string input_K = Console.ReadLine();
+        int input_int_K = Convert.ToInt16(input_K);
+        int all_K_int = 0;
+        int_M -= 1;
+        for (int M_len = 0; M_len <= int_M; M_len++)
+        {
+            all_K_int = all_K_int + num_array[M_len, input_int_K];
+        }
+        Console.WriteLine($"Колличество дет: {int_M}, сред значене {all_K_int/ int_M}");
+    }
+}
---
+++ repos/myApp1/myApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задание 1, калькулятор для алгебраических действий");
+        Console.WriteLine("Введите 1 число");
+        string? string_num1 = Console.ReadLine();
+        Console.WriteLine("Введите 2 число");
+        string? string_num2 = Console.ReadLine();
+        Console.WriteLine("Введите алгебраическое действие");
+        string? string_operaitor = Console.ReadLine();
+
+        int final_num = 0;
+
+        try
+        {
+            int num1 = Convert.ToInt16(string_num1);
+            int num2 = Convert.ToInt16(string_num2);
+
+            char operaitor = Convert.ToChar(string_operaitor);
+
+            if (operaitor == '-')
+            {
+                final_num = num1 - num2;
+            }
+            else if (operaitor == '+')
+            {
+                final_num = num1 + num2;
+            }
+            else if (operaitor == '/')
+            {
+                final_num = num1 / num2;
+            }
+            else if (operaitor == '*')
+            {
+                final_num = num1 * num2;
+            }
+            Console.WriteLine("Ответ: " + final_num);
+        }
+        catch
+        {
+            Console.WriteLine("err operator or num");
+        }
+
+    }
+}
